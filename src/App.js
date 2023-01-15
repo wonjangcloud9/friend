ca25c1eb35dd45a366b17d40d22b55308bbdeef1
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-15 14:55:51 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import main from "./main.jpg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [mainImg, setMainImg] = useState(true);
+  const clickImg = () => {
+    setMainImg(!mainImg);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" onClick={clickImg}>
+        <img
+          src={main}
+          className={mainImg ? "App-logo" : "App-logo2"}
+          alt="logo"
+          style={{
+            width: "50vw",
+            height: "50vw",
+            objectFit: "cover",
+            borderRadius: "50%",
+          }}
+        />
+        {mainImg ? (
+          <div>
+            <p>안녕하세요. 제가 누군지 궁금하신가요?</p>
+            <p>궁금하시면 이미지를 클릭하세요.</p>
+          </div>
+        ) : (
+          <div>
+            <p>저는 유승훈이에요.</p>
+            <p>같이 놀러갈까요?</p>
+          </div>
+        )}
       </header>
     </div>
   );
